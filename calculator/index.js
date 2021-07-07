a7964e235e304e04077f5ec3abbb9ea8b130d6d2
Raymond <raymond.lo@yahoo.com>
2021-07-07 01:29:38 -0400

    Logic to display operator buttons

--- calculator/index.js
+++ calculator/index.js
@@ -9,6 +9,7 @@ class Calculator {
 
   updateScreen() {
     this.inputDisplay.textContent = this.inputState;
+    this.historyDisplay.textContent = this.historyState;
   }
 }
 
@@ -26,37 +27,76 @@ class Button {
   }
 }
 
+class OperatorButton {
+  constructor(el) {
+    this.el = el;
+    el.addEventListener("click", this.handleOperatorClick.bind(this));
+  }
+
+  handleOperatorClick() {
+    const operators = ["+", "-", "x", "÷"];
+    const lastInput = calculator.inputState.slice(-1);
+    // prevent adding another operator if last character is an operator
+    if (operators.includes(lastInput)) return;
+
+    calculator.inputState += this.el.textContent;
+    calculator.updateScreen();
+  }
+}
+
+class ClearButton {
+  constructor(el) {
+    this.el = el;
+    el.addEventListener("click", this.clearInput.bind(this));
+  }
+
+  clearInput() {
+    calculator.inputState = "0";
+    calculator.updateScreen();
+  }
+}
+
+class EqualButton {
+  constructor(el) {
+    el.addEventListener("click", this.calculateInput.bind(this));
+  }
+
+  calculateInput() {
+    console.log("calculate");
+  }
+}
+
 class DeleteButton {
   constructor(el) {
     this.el = el;
+    el.addEventListener("click", this.deleteLastInput.bind(this));
   }
 
-  deleteLastInput() {}
-}
+  deleteLastInput() {
+    let newInput = calculator.inputState.slice(0, -1);
+    if (newInput.length < 1) newInput = "0";
 
-class AddButton extends Button {
-  add() {}
-}
-
-class SubtractButton extends Button {
-  subtract() {}
-}
-
-class DivideButton extends Button {
-  divide() {}
-}
-
-class MultiplyButton extends Button {
-  multiply() {}
+    calculator.inputState = newInput;
+    calculator.updateScreen();
+  }
 }
 
 const screenNode = document.querySelector(".screen");
 const calculator = new Calculator(screenNode);
 
 const deleteNode = document.querySelector('[data-button-type="delete"]');
-const deleteBtn = new DeleteButton(deleteNode);
+new DeleteButton(deleteNode);
+
+const clearButton = document.querySelector('[data-button-type="clear"]');
+new ClearButton(clearButton);
+
+const operatorButtons = document.querySelectorAll(
+  '[data-button-type="operator"]'
+);
+operatorButtons.forEach((btn) => new OperatorButton(btn));
 
 const digitBtns = document.querySelectorAll('[data-button-type="digit"]');
-digitBtns.forEach((btn) => {
-  new Button(btn);
-});
+digitBtns.forEach((btn) => new Button(btn));
+
+const equalBtn = document.querySelector('[data-button-type="equal"]');
+new EqualButton(equalBtn);
