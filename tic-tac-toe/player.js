e7dd1eab07bed0cd714aad2c2252ca161fbd472f
Raymond <raymond.lo@yahoo.com>
2021-07-23 01:26:26 -0400

    added name variable and public get name method

--- tic-tac-toe/player.js
+++ tic-tac-toe/player.js
@@ -1,11 +1,17 @@
-const playerFactory = (marker) => {
+const playerFactory = (marker, name) => {
   const _marker = marker;
+  const _name = name;
 
   const getMarker = () => {
     return _marker;
   };
 
+  const getName = () => {
+    return _name;
+  };
+
   return {
     getMarker,
+    getName,
   };
 };
